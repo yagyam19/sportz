04d4965374580a8b81f960858a7d03aad194cf60
Yagyam Patidar <ypatidar@grepruby.io>
2026-02-08 20:16:25 +0530

    implemented the websocket server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import http from 'http';
 import express from 'express';
 import { matchRouter } from './routes/matches.js';
+import { attachWebSocketServer } from './ws/server.js';
+
+const PORT = Number(process.env.PORT || 8000);
+const HOST = process.env.HOST || '0.0.0.0';
 
 const app = express();
-const port = 8000;
+
+const server = http.createServer(app);
 
 app.use(express.json());
 
@@ -12,6 +18,13 @@ app.get('/', (req, res) => {
 
 app.use('/matches', matchRouter);
 
-app.listen(port, (req, res) => {
-  console.log(`Server is running on http://localhost:${port}`);
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
+server.listen(PORT, HOST, () => {
+  const baseUrl =
+    HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+  console.log(`Server is running on ${baseUrl}`);
+  console.log(`Websocket server is running on ${baseUrl.replace('http', 'ws')}/ws`)
 });
